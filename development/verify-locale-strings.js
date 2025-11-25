a8e12ac5f22f43d7ff4c63759161676dc2e8fb98
georgewrmarshall <george.marshall@consensys.net>
2025-11-25 20:42:21 +1300

    chore: lint fix

--- development/verify-locale-strings.js
+++ development/verify-locale-strings.js
@@ -40,7 +40,6 @@ const {
 const readFile = promisify(fs.readFile);
 const writeFile = promisify(fs.writeFile);
 
-
 // Build and compile a single regex from all exceptions for performance
 function buildExceptionsRegex(exceptions) {
   const patterns = [];
