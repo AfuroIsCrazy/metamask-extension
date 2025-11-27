7088b4142b48010fc3ed9dd0b57621d6ff4e48ca
Micaela Estabillo <micaela.estabillo@consensys.net>
2025-11-26 16:58:51 -0800

    chore: require and use assetId to fetch exchange rates

--- ui/ducks/bridge/selectors.ts
+++ ui/ducks/bridge/selectors.ts
@@ -74,7 +74,6 @@ import {
   HardwareKeyringNames,
   HardwareKeyringType,
 } from '../../../shared/constants/hardware-wallets';
-import { toAssetId } from '../../../shared/lib/asset-utils';
 import { MULTICHAIN_NATIVE_CURRENCY_TO_CAIP19 } from '../../../shared/constants/multichain/assets';
 import { Numeric } from '../../../shared/modules/Numeric';
 import { getIsSmartTransaction } from '../../../shared/modules/selectors';
@@ -628,10 +627,7 @@ export const getFromTokenConversionRate = createSelector(
       const nativeAssetId = getNativeAssetForChainId(
         fromChain.chainId,
       )?.assetId;
-      const tokenAssetId = toAssetId(
-        fromToken.address,
-        formatChainIdToCaip(fromChain.chainId),
-      );
+      const tokenAssetId = fromToken.assetId;
       const nativeToCurrencyRate = isNonEvmChain(fromChain.chainId)
         ? Number(
             rates?.[fromChain.nativeCurrency?.toLowerCase()]?.conversionRate ??
